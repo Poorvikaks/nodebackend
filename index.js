427505812951dbbb22efd7a56fa2a69fec3d5684
Dhanushree R <dhanushree@agilehealthtech.com>
2026-01-13 15:34:21 +0530

    data added

--- index.js
+++ index.js
@@ -3,6 +3,41 @@ console.log(process.env)
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000;
+const githubdata = {
+  "login": "Poorvikaks",
+  "id": 112681562,
+  "node_id": "U_kgDOBrdiWg",
+  "avatar_url": "https://avatars.githubusercontent.com/u/112681562?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/Poorvikaks",
+  "html_url": "https://github.com/Poorvikaks",
+  "followers_url": "https://api.github.com/users/Poorvikaks/followers",
+  "following_url": "https://api.github.com/users/Poorvikaks/following{/other_user}",
+  "gists_url": "https://api.github.com/users/Poorvikaks/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/Poorvikaks/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/Poorvikaks/subscriptions",
+  "organizations_url": "https://api.github.com/users/Poorvikaks/orgs",
+  "repos_url": "https://api.github.com/users/Poorvikaks/repos",
+  "events_url": "https://api.github.com/users/Poorvikaks/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/Poorvikaks/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": null,
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 10,
+  "public_gists": 0,
+  "followers": 1,
+  "following": 1,
+  "created_at": "2022-09-02T05:30:08Z",
+  "updated_at": "2024-05-01T08:27:49Z"
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -15,6 +50,9 @@ app.get('/login',(req,res)=>{
 })
 app.get('/youtube',(req,res)=>{
     res.send('<h2>coding set </h2>')
+})
+app.get('/github', (req,res)=>{
+  res.json(githubdata)
 })
 
 app.listen(PORT, () => {
